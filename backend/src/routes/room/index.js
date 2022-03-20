e2b4c4f2a247a6711f2b91980c8214d0fe695985
Blake Johnson <blake.k.johnson.4@gmail.com>
2022-03-19 21:18:19 -0400

    added room router

--- backend/src/routes/room/index.js
+++ backend/src/routes/room/index.js
@@ -1,11 +1,61 @@
 import { Router } from 'express'
+import { Building, Room } from '../../store/models'
 
 const roomRouter = Router()
 
-roomRouter.get('/', (req, res) => {
-  res.send('room')
+roomRouter.post('/', async (req, res) => {
+  const roomData = req.body
+
+  try {
+    // Check that the building exists
+    const building = await Building.findById(roomData.building)
+    if (!building)
+      return res.status(400).send({ message: 'No such building exists' })
+
+    // Create the room
+    const room = new Room(roomData)
+    await room.save()
+
+    building.rooms.push(room.id)
+    await building.save()
+
+    return res.status(200).send({ id: room.id })
+  } catch (e) {
+    return res.status(500).send({ message: e.message })
+  }
 })
 
-roomRouter.post('/', (req, res) => {})
+roomRouter.get('/', async (req, res) => {
+  try {
+    const rooms = await Room.find({}).exec()
+    return res.status(200).send({ items: rooms })
+  } catch (e) {
+    return res.status(500).send({ message: e.message })
+  }
+})
+
+roomRouter.get('/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const room = await Room.findById(id).exec()
+    if (!room) return res.status(400).send({ message: 'No such room exists' })
+
+    return res.status(200).send(room)
+  } catch (e) {
+    return res.status(500).send({ message: e.message })
+  }
+})
+
+roomRouter.delete('/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    await Room.findByIdAndRemove(id).exec()
+    return res.status(200).send({ message: 'Deleted room' })
+  } catch (e) {
+    return res.status(500).send({ message: e.message })
+  }
+})
 
 export default roomRouter
